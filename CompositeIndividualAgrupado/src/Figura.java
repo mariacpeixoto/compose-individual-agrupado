8ad8891760c20ad681110309716ac0db01a382d8
Maria Peixoto <83721653+mariacpeixoto@users.noreply.github.com>
2024-09-27 21:03:14 -0300

    Add files via upload

---
+++ CompositeIndividualAgrupado/src/Figura.java
@@ -0,0 +1,5 @@
+public interface Figura {
+	void desenha();
+    void mudaCorDeFundo(Cor cor);
+}
+
---
+++ CompositeIndividualAgrupado/src/Triangulo.java
@@ -0,0 +1,9 @@
+public class Triangulo implements Figura {
+	 public void desenha() {
+	     System.out.println("Desenhando um triângulo.");
+	 }
+	 
+	 public void mudaCorDeFundo(Cor cor) {
+	     System.out.println("Mudando a cor de fundo do triângulo para: " + cor);
+	 }
+}
